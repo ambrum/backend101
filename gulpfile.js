eee96fa8c3d92b0ed14bdbe22c0c48f3a4721846
ambrum <irina.ambrumyan@gmail.com>
2016-11-04 12:18:35 +0400

    04.11

--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,23 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    clean = require('gulp-clean'),
-    browserSync = require('browser-sync').create(),
-    child_process = require('child_process'),
-    livereload = require('gulp-livereload');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
 
 
-// Styles
-gulp.task('styles', function() {
-    return gulp.src('styles/main.scss')
-        .pipe(sass({ style: 'expanded' }))
-        .pipe(livereload(server))
-        .pipe(gulp.dest('dist/styles'))
-        .pipe(notify({ message: 'Styles task complete' }));
-});
-gulp.task('sass:watch', function() {
-    return gulp.watch('media/styles/sass/**/*.scss', ['sass'])
-        .on('change', function(event) {
-            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
+gulp.task('watch', function() {
+    gulp.watch('app/media/src/styles/**/*.scss', ['sass']);
 });
 
-// Scripts
-gulp.task('scripts', function() {
-    return gulp.src('src/scripts/**/*.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
-        .pipe(livereload(server))
-        .pipe(gulp.dest('dist/scripts'))
-        .pipe(notify({ message: 'Scripts task complete' }));
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass())
+        .pipe(gulp.dest('app/styles'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
-
-// Images
-gulp.task('images', function() {
-    return gulp.src('src/images/**/*')
-        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(livereload(server))
-        .pipe(gulp.dest('dist/images'))
-        .pipe(notify({ message: 'Images task complete' }));
-});
-
-// // Clean
-gulp.task('clean', function() {
-    return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false})
-        .pipe(clean());
-});
-
-gulp.task('server:restart', function () {
-    if (process.node) process.node.kill();
-
-    process.node = child_process.spawn('node', ['app.js'], {stdio: 'inherit'});
-});
-
-// Default task
-// gulp.task('default', ['clean'], function() {
-//     gulp.run('styles', 'scripts', 'images');
-// });
-
-gulp.task('default', ['server:restart', 'sass:watch'], function () {
-    browserSync.init({
-        proxy: 'http://localhost:3000',
-        port: 3001,
-        ui: false,
-        open: false
-    });
-
-    gulp.watch('media/**/*.css', function (file) {
-        gulp.src(file.path).pipe(browserSync.stream());
-    });
-});
-
-// Watch
-// gulp.task('watch', function() {
-//
-//     // Listen on port 35729
-//     server.listen(35729, function (err) {
-//         if (err) {
-//             return console.log(err)
-//         };
-//
-//         // Watch .scss files
-//         gulp.watch('src/styles/**/*.scss', function(event) {
-//             console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-//             gulp.run('styles');
-//         });
-//
-//         // Watch .js files
-//         gulp.watch('src/scripts/**/*.js', function(event) {
-//             console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-//             gulp.run('scripts');
-//         });
-//
-//         // // Watch image files
-//         // gulp.watch('src/images/**/*', function(event) {
-//         //     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-//         //     gulp.run('images');
-//         // });
-//
-//     });
-//
-// });
+gulp.task('default', function (callback) {
+    runSequence(['sass', 'watch'],
+        callback
+    )
+})
